7dc590371390ad8cc7dbb7ddedcb1fe9aa12bafe
DESKTOP-V7NPBRQ\User <carlosquispe9876@gmail.com>
2023-03-08 23:22:44 -0500

    geolocalizacion

---
+++ Geolocalizacion/Geolocalizacion/Geo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geolocalizacion
+{
+    public class Geo
+    {
+        public double latitud { get; set; }
+        public double longitud { get; set; }
+    }
+}
--- Geolocalizacion/Geolocalizacion/MainPage.xaml.cs
+++ Geolocalizacion/Geolocalizacion/MainPage.xaml.cs
@@ -1,18 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Geolocalizacion
 {
     public partial class MainPage : ContentPage
     {
+        CancellationTokenSource cts;
+        ObservableCollection<Geo> geoList= new ObservableCollection<Geo>();
         public MainPage()
         {
             InitializeComponent();
+            Lista.ItemsSource = geoList;
+        }
+
+        public ObservableCollection<Geo> GeoList { get { return geoList; } }
+
+        async Task GetCurrentLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Low, TimeSpan.FromSeconds(10));
+                cts = new CancellationTokenSource();
+                var location = await Geolocation.GetLocationAsync(request, cts.Token);
+
+                if (location != null)
+                {
+                    latitud.Text = location.Latitude.ToString();
+                    longitud.Text = location.Longitude.ToString();
+                    geoList.Add(new Geo { latitud=location.Latitude, longitud=location.Longitude });
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+        }
+
+        protected override void OnDisappearing()
+        {
+            if (cts != null && !cts.IsCancellationRequested)
+                cts.Cancel();
+            base.OnDisappearing();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await GetCurrentLocation();
+        }
+
+        private void Clean_Clicked(object sender, EventArgs e)
+        {
+            latitud.Text = "";
+            longitud.Text = "";
+            geoList.Clear();
         }
     }
 }
